ed7858c6a8f25158acda1397061abfd88b2f19da
thiago lopes de mello <timabuntu@gmail.com>
2022-01-06 16:30:17 -0300

    chore: Comment populado e estilizado

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,8 +1,13 @@
-function Comment() {
+function Comment({ comment }) {
 	return (
-		<section className='bg-purple-200'>
-			<img src='' alt='' />
-			<p>resposta!</p>
+		<section className='flex py-3 h-auto mt-2'>
+			<div className='flex -mb-4'>
+				<img className='rounded-full h-10' src={comment.author.avatar} alt='' />
+				<p className='bg-gray-200 ml-3 rounded-3xl p-3 text-gray-900'>
+					<strong className='mr-1'>{comment.author.name}</strong>
+					{comment.content}
+				</p>
+			</div>
 		</section>
 	);
 }
