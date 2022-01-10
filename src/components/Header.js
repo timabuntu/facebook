fa8c8d6fbee29a78313ef00be23687e43be382d5
Thiago Lopes de mello <timabuntu@gmail.com>
2022-01-10 15:43:57 -0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import Facebook from "../assets/facebook.png";
 
 function Header() {
 	return (
-		<header className='sticky top-0 flex items-center w-full h-16 bg-blue-400 sm:'>
+		<header className='sticky top-0 flex items-center w-full h-16 bg-blue-400'>
 			<img src={Facebook} alt='facebook' className='h-5 ml-4' />
 		</header>
 	);
